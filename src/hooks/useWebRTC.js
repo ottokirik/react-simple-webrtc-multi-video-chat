f7f701ade9ccc0edc1fa426991bab36368939444
Petrov Kirill <ottokirik@gmail.com>
2021-10-01 00:31:10 +0300

    Created hooks for WebRTC

---
+++ src/hooks/useWebRTC.js
@@ -0,0 +1,56 @@
+import { useCallback, useEffect, useRef } from 'react';
+import { socket } from 'socket';
+import { JOIN } from 'socket/actions';
+import { useStateWithCallback } from './useStateWithCallback';
+
+export const LOCAL_VIDEO = 'LOCAL_VIDEO';
+
+export const useWebRTC = ({ roomID }) => {
+  const [clients, setClients] = useStateWithCallback([]);
+
+  const addNewClient = useCallback(
+    (newClient, cb) => {
+      if (!clients.includes(newClient)) {
+        setClients((list) => [...list, newClient], cb);
+      }
+    },
+    [clients, setClients]
+  );
+
+  const peerConnections = useRef({});
+  const localMediaStream = useRef(null);
+  const peerMediaElements = useRef({
+    [LOCAL_VIDEO]: null,
+  });
+
+  useEffect(() => {
+    const startCapture = async () => {
+      localMediaStream.current = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: {
+          width: 1280,
+          height: 720,
+        },
+      });
+
+      addNewClient(LOCAL_VIDEO, () => {
+        const localVideoElement = peerMediaElements.current[LOCAL_VIDEO];
+
+        if (localVideoElement) {
+          localVideoElement.volume = 0;
+          localVideoElement.srcObj = localMediaStream.current;
+        }
+      });
+    };
+
+    startCapture()
+      .then(() => socket.emit(JOIN, { room: roomID }))
+      .catch((e) => console.log('Error getting user media', e));
+  }, [roomID]);
+
+  const provideMediaRef = useCallback((id, node) => {
+    peerMediaElements.current[id] = node;
+  }, []);
+
+  return { clients, provideMediaRef };
+};
--- src/pages/room/Room.jsx
+++ src/pages/room/Room.jsx
@@ -1,3 +1,22 @@
+import { LOCAL_VIDEO, useWebRTC } from 'hooks/useWebRTC';
+import { useParams } from 'react-router';
+
 export const Room = () => {
-  return <div>Room</div>;
+  const { id: roomID } = useParams();
+  const { clients, provideMediaRef } = useWebRTC({ roomID });
+
+  return (
+    <div>
+      {clients.map((clientID) => (
+        <div key={clientID}>
+          <video
+            ref={(instance) => provideMediaRef(clientID, instance)}
+            autoPlay
+            playsInline
+            muted={clientID === LOCAL_VIDEO}
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
